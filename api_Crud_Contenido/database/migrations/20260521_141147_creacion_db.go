5096234b8869c7c19185230887002b2e7581a9c8
Katha Gualdrón <gualdronmarquezw2008@gmail.com>
2026-05-21 14:25:03 -0500

    Se generan las migraciones usando bee migrate y se digitan los códigos sql para insertar y borrar la base de datos.

---
+++ api_Crud_Contenido/database/migrations/20260521_141147_creacion_db.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"github.com/beego/beego/v2/client/orm/migration"
+)
+
+// DO NOT MODIFY
+type CreacionDb_20260521_141147 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &CreacionDb_20260521_141147{}
+	m.Created = "20260521_141147"
+
+	migration.Register("CreacionDb_20260521_141147", m)
+}
+
+// Run the migrations
+func (m *CreacionDb_20260521_141147) Up() {
+	file, err := ioutil.ReadFile("../scripts/20260521_141147_creacion_db_up.sql")
+	if err != nil {
+		fmt.Println(err)
+	}
+	requests := strings.Split(string(file), ";")
+
+	for _, request := range requests {
+		fmt.Println(request)
+		m.SQL(request)
+	}
+}
+
+// Reverse the migrations
+func (m *CreacionDb_20260521_141147) Down() {
+	file, err := ioutil.ReadFile("../scripts/20260521_141147_creacion_db_down.sql")
+	if err != nil {
+		fmt.Println(err)
+	}
+	requests := strings.Split(string(file), ";")
+
+	for _, request := range requests {
+		fmt.Println(request)
+		m.SQL(request)
+	}
+}
