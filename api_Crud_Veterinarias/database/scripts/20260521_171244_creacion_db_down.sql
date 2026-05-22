d9e8a212171e4431233f3e9224d8738365709678
duconguaidia-spec <duconguaidia@gmail.com>
2026-05-22 13:53:51 -0500

    se agrega en el script creacion table up para crear el schema pedidos y la tabla Noticias con sus columnas y restricciones

---
+++ api_Crud_Veterinarias/database/scripts/20260521_171244_creacion_db_down.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS Veterinarias.ResenaVeterinaria;
+DROP TABLE IF EXISTS Veterinarias.ProductoVeterinaria;
+DROP TABLE IF EXISTS Veterinarias.ServicioVeterinaria;  
+DROP TABLE IF EXISTS Veterinarias.tr_VeterinariaServicio;
+DROP TABLE IF EXISTS Veterinarias.tr_VeterinariaEspecialidad;
+DROP TABLE IF EXISTS Veterinarias.Veterinaria;
+DROP TABLE IF EXISTS Veterinarias.Especialidad;
+DROP TABLE IF EXISTS Veterinarias.ServicioGeneral;
+
+DROP SCHEMA IF EXISTS Veterinarias;
