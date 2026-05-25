8555e9a2551822e9db4dfa57aaee2a33db25bda6
mauricioponguta <pongutaerin18@gmail.com>
2026-05-25 11:29:21 -0500

    Configura arranque con variables de entorno

--- api_Crud_agropecuario/go.mod
+++ api_Crud_agropecuario/go.mod
@@ -5,6 +5,7 @@ go 1.26.2
 require (
 	github.com/beego/bee/v2 v2.3.0
 	github.com/beego/beego/v2 v2.3.10
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.12.3
 )
 
--- api_Crud_agropecuario/main.go
+++ api_Crud_agropecuario/main.go
@@ -3,22 +3,83 @@ package main
 import (
 	_ "API_CRUD_AGROPECUARIO/routers"
 
-	beego "github.com/beego/beego/v2/server/web"
-	beeLogger "github.com/beego/bee/v2/logger"
 	"github.com/beego/beego/v2/client/orm"
+	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/filter/cors"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	sqlConn,err := beego.AppConfig.String("sqlconn")
+
+	// Cargar variables del .env
+	err := godotenv.Load()
 	if err != nil {
-		beeLogger.Log.Fatal("%s")
+		panic("Error cargando el archivo .env")
 	}
-	orm.RegisterDataBase("default", "postgres", sqlConn)
+
+	// Recargar app.conf después del .env
+	err = beego.LoadAppConfig("ini", "conf/app.conf")
+	if err != nil {
+		panic(err)
+	}
+
+	// Obtener variables de PostgreSQL
+	pgUser, _ := beego.AppConfig.String("PGuser")
+	pgPass, _ := beego.AppConfig.String("PGpass")
+	pgHost, _ := beego.AppConfig.String("PGhost")
+	pgPort, _ := beego.AppConfig.String("PGport")
+	pgDb, _ := beego.AppConfig.String("PGdb")
+	pgSchema, _ := beego.AppConfig.String("PGschema")
+
+	// Registrar base de datos
+	err = orm.RegisterDataBase(
+		"default",
+		"postgres",
+		"postgres://"+
+			pgUser+":"+
+			pgPass+"@"+
+			pgHost+":"+
+			pgPort+"/"+
+			pgDb+
+			"?sslmode=disable&search_path="+
+			pgSchema,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Swagger en modo dev
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	// Configuración CORS
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{
+			"PUT",
+			"PATCH",
+			"GET",
+			"POST",
+			"OPTIONS",
+			"DELETE",
+		},
+		AllowHeaders: []string{
+			"Origin",
+			"x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken",
+		},
+		ExposeHeaders:    []string{"content-length"},
+		AllowCredentials: true,
+	}))
+
+	// Ejecutar servidor
 	beego.Run()
 }
-
